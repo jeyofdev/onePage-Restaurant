b8bc2dadfe6dde2ff58c897562c4a099e08dc47a
jeyofdev <jgregoire.pro@gmail.com>
2019-05-20 17:25:58 +0200

    set the overlay of the responsive nav

--- src/js/app/nav-scroll.js
+++ src/js/app/nav-scroll.js
@@ -9,7 +9,7 @@ export function navScroll () {
       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']')
       if (target.length) {
         $('html, body').animate({
-          scrollTop: (target.offset().top - 56)
+          scrollTop: (target.offset().top)
         }, 1000, 'easeInOutExpo')
         return false
       }
@@ -23,7 +23,6 @@ export function navScroll () {
 
   // activate scrollspy to add active class to navbar items on scroll
   $('body').scrollspy({
-    target: '#mainNav',
-    offset: 56
+    target: '#mainNav'
   })
 }
--- src/js/app/toggle-button.js
+++ src/js/app/toggle-button.js
@@ -4,11 +4,15 @@ export function toggleButton () {
 }
 
 /**
- * Manage the responsive button
+ * Manage the responsive nav
  */
 class ToggleButton {
   constructor () {
+    this.windowWidth = window.innerWidth
     this.button = document.querySelector('#navToggle') // responsive button
+    this.navbarCollapse = document.querySelector('#navbarResponsive')
+    this.navbarNav = document.querySelector('.navbar-nav')
+    this.links = document.querySelectorAll('.js-scroll-trigger') // nav links
   }
 
   /**
@@ -17,27 +21,77 @@ class ToggleButton {
   init () {
     this.collapse()
     this.resize()
+
+    if (this.windowWidth >= 992) {
+      this.setOverlay(false)
+    } else {
+      this.setOverlay()
+      this.links.forEach((currentValue, currentIndex) => {
+        this.links[currentIndex].addEventListener('click', () => {
+          this.button.classList.remove('is-active')
+        })
+      })
+    }
   }
 
   /**
-   * activate the menu responsive button
+   * activate/disable the menu responsive button
+   * and show/hide the overlay with an animation
    */
   collapse () {
     this.button.addEventListener('click', (e) => {
       this.button.classList.toggle('collapsed')
       this.button.classList.toggle('is-active')
+
+      if (!this.button.classList.contains('is-active')) {
+        this.navbarCollapse.style.animation = 'fadeOutLeft .5s forwards'
+      } else {
+        this.navbarCollapse.style.animation = 'fadeInLeft .5s forwards'
+      }
+
+      if (this.navbarCollapse.classList.contains('overlay')) {
+        this.navbarCollapse.classList.toggle('hide')
+      } else {
+        this.navbarCollapse.classList.add('hide')
+      }
     })
   }
 
   /**
    * disable responsive menu button on large screens
+   *
    */
   resize () {
     window.onresize = () => {
-      if (window.innerWidth >= 992) {
+      this.windowWidth = window.innerWidth
+
+      if (this.windowWidth >= 992) {
         this.button.classList.remove('is-active')
         this.button.classList.add('collapsed')
+        this.setOverlay(false)
+        this.navbarCollapse.classList.remove('show')
+      } else {
+        this.setOverlay(true)
       }
+    }
+  }
+
+  /**
+   * add the overlay in mobile mode
+   *
+   * @param {Boolean} mobile
+   */
+  setOverlay (mobile = true) {
+    if (mobile) {
+      this.navbarCollapse.classList.add('overlay')
+      this.navbarNav.classList.add('overlay-content')
+      this.navbarCollapse.style.background = '#000'
+    } else {
+      this.navbarCollapse.classList.remove('overlay')
+      this.navbarNav.classList.remove('overlay-content')
+      this.navbarCollapse.style.background = 'transparent'
+      this.navbarCollapse.style.height = 'auto'
+      this.navbarCollapse.style.animation = 'none'
     }
   }
 }
