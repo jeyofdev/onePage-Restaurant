001d6274c3674597ed821f4c46a33684eb9e3940
jeyofdev <jgregoire.pro@gmail.com>
2019-05-29 12:52:07 +0200

    fix: fix the display problem when clicking on a link of the nav

--- src/js/app/NavScroll.js
+++ src/js/app/NavScroll.js
@@ -29,14 +29,16 @@ export class NavScroll {
    * smooth scrolling using jQuery easing
    */
   smoothScrolling () {
+    var that = this
+
     $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
       if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
         var target = $(this.hash)
+        var targetId = $(this).attr('href')
         target = target.length ? target : $('[name=' + this.hash.slice(1) + ']')
+
         if (target.length) {
-          $('html, body').animate({
-            scrollTop: (target.offset().top)
-          }, 1000, 'easeInOutExpo')
+          that.responsiveAnimateScrolling(target, targetId)
           return false
         }
       }
@@ -59,5 +61,64 @@ export class NavScroll {
     $('body').scrollspy({
       target: this.target
     })
+  }
+
+  /**
+   * set the smooth scrolling animation depending on the size of the screen
+   * @param {object} target the target of the clicked link
+   * @param {string} targetId the id of the target
+   */
+  responsiveAnimateScrolling (target, targetId) {
+    let navHeight = $('#mainNav').outerHeight()
+    let screenWidth = ($(document).outerWidth(true))
+
+    if (screenWidth < 992) {
+      if (targetId !== '#page-top') {
+        navHeight = 0
+        this.animateScrolling(target, 1000, 'easeInOutExpo', navHeight)
+      } else {
+        this.animateScrolling(target, 1000, 'easeInOutExpo', navHeight)
+      }
+    } else {
+      if (navHeight === 105) {
+        if (targetId !== '#page-top') {
+          navHeight = 0
+          this.animateScrolling(target, 1000, 'easeInOutExpo', navHeight)
+        } else {
+          this.animateScrolling(target, 1000, 'easeInOutExpo', navHeight)
+        }
+      } else {
+        if (targetId !== '#page-top') {
+          navHeight = 0
+          this.animateScrolling(target, 1000, 'easeInOutExpo', navHeight)
+        } else {
+          navHeight = navHeight + this.convertEmToPx(1.25)
+          this.animateScrolling(target, 1000, 'easeInOutExpo', navHeight)
+        }
+      }
+    }
+  }
+
+  /**
+   * set the smooth scrolling animation
+   * @param {object} target the target of the clicked link
+   * @param {number} duration the duration of the animation
+   * @param {string} easing the easing
+   * @param {number} navHeight the height of the nav
+   */
+  animateScrolling (target, duration, easing, navHeight) {
+    $('html, body').animate({
+      scrollTop: (target.offset().top - navHeight)
+    }, duration, easing)
+  }
+
+  /**
+   * convert em to px
+   * @param {number} base default value of the browser (16px)
+   * @param {number} value value in em to convert
+   */
+  convertEmToPx (value, base = 16) {
+    var px = value * base
+    return px
   }
 }
